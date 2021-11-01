61ac2d975570241bdfe1b4a4ca8b202b53379b61
Zarif Mahfuz <mahfuz@ualberta.ca>
2021-10-31 22:23:48 -0600

    Resolves #84 and #82

--- code/HabitTracker/app/src/main/java/com/example/habittracker/activities/profile/ProfileFollowRequestsActivity.java
+++ code/HabitTracker/app/src/main/java/com/example/habittracker/activities/profile/ProfileFollowRequestsActivity.java
@@ -6,7 +6,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.example.habittracker.NavBarManager;
 import com.example.habittracker.R;
 
 public class ProfileFollowRequestsActivity extends AppCompatActivity {
@@ -15,7 +14,7 @@ public class ProfileFollowRequestsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_follow_requests);
-        Button back_button = findViewById(R.id.followingButton);
+        Button back_button = findViewById(R.id.back);
         back_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
